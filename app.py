7887a01fc5df6edb7e7c9d2913c27251e2112f6a
jovif <rmjoaovictor@gmail.com>
2017-04-10 23:30:02 -0300

    main app file

--- app.py
+++ app.py
@@ -2,23 +2,16 @@ from flask import Flask
 from flask import request, abort, render_template, make_response, jsonify, json
 from ApiWrapper.wrapper import Wrapper
 
+
 app = Flask(__name__)
 ApiWrapper = Wrapper('config/config.json')
-ApiWrapper.reloadFiles()
-ApiWrapper.observe();
+ApiWrapper.observe()
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/teste')
-def teste():
-    response = app.response_class(
-        response=ApiWrapper.getFile('CAD'),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
 
 @app.route('/currencies')
 def currencies():
@@ -28,14 +21,38 @@ def currencies():
         mimetype='application/json'
     )
 
-@app.route('/template/index')
-def index_template():
-    with open('templates/template_index.html', 'r') as _file:
+
+@app.route('/template/<name>')
+def index_template(name=''):
+    with open('templates/template_' + name + '.html', 'r') as _file:
         return app.response_class(
             response=_file.read(),
             status=200,
             mimetype='text/plain'
         )
+
+
+@app.route('/today/<name>')
+def live(name=None):
+    if name:
+        with open('model/TODAY.txt', 'r') as todayCurrency:
+            myJson = json.load(todayCurrency)
+            requestName = 'USD'+name.upper()
+            if requestName in myJson['quotes']:
+                return app.response_class(
+                    response=json.dumps(
+                            {
+                                "value": str(myJson['quotes'][requestName]),
+                                "name": name.upper()
+                            }
+                        ),
+                    status=200,
+                    mimetype='application/json'
+                )
+            else:
+                return 'name not found', 404
+    abort(404)
+
 
 @app.route('/chart/')
 @app.route('/chart/<name>')
@@ -52,11 +69,10 @@ def chart(name=None):
 
     return '', 204
 
+
 @app.errorhandler(404)
 def not_found(error):
     return render_template("error.html", error=error), 404
 
 
-if __name__ == "__main__":
-    #app.run(host="0.0.0.0", port=8080, threaded=True)
-    app.run(threaded=True, debug=True)
+app.run(threaded=True, debug=True)
